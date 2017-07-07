34f45a2250cb13dc9fe09bf27bd2146b3d2cd4f6
Abi Raja <abimanyuraja@gmail.com>
2017-07-06 20:27:50 -0700

    clean up WebScreen.js

--- screens/WebScreen.js
+++ screens/WebScreen.js
@@ -1,38 +1,20 @@
 import React from 'react';
-import {
-  Text,
-  Image,
-  View,
-  ListView,
-  TouchableHighlight,
-  Platform,
-  StyleSheet,
-  Dimensions,
-  WebView
-} from 'react-native';
-import { LinearGradient } from 'expo';
-import { Card, CardImage } from 'react-native-card-view';
+import { Platform, WebView } from 'react-native';
 
 class WebScreen extends React.Component {
-
   static navigationOptions = {
     title: 'Return to Week Details',
     headerTintColor: 'white',
     headerStyle: {
-      backgroundColor:'#821c21',
+      backgroundColor: '#821c21',
       paddingTop: 20,
-      height: (Platform.OS === 'ios') ? 80 : 100,
-    }
+      height: Platform.OS === 'ios' ? 80 : 100,
+    },
   };
 
   render() {
     const { params } = this.props.navigation.state;
-    return (
-      <WebView
-        source={{ uri: params.url }}
-        style={{ marginTop: 0 }}
-      />
-    );
+    return <WebView source={{ uri: params.url }} style={{ marginTop: 0 }} />;
   }
 }
 
