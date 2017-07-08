4bc869888741ce8e4083d7ecaa60ff29f1eba5d4
Jeffrey Da <jexelD@gmail.com>
2017-07-07 17:38:54 -0700

    Split up XML processing into a separate module that transforms XML to JSON (Fix #2)

--- screens/WeekScreen.js
+++ screens/WeekScreen.js
@@ -38,19 +38,43 @@ class WeekScreen extends React.Component {
     };
   }
 
+  _createJSON(data) {
+    var json = {};
+    for (var i = 0; i < data.length - 1; i++) {
+      if (i < 2) {
+        json[data[i].name] = data[i].value;
+        json[data[i].name] = data[i].value;
+      } else {
+        var title = data[i].children[0].value;
+        var link = data[i].children[1].attributes.href;
+        json[title] = link;
+      }
+    }
+    var videos = {};
+    var videoData = data[data.length - 1].children[1].children;
+    for (var i = 1; i < videoData.length; i++) {
+      var link = videoData[i].attributes.href;
+      var type = videoData[i].children[0].value;
+      videos[type] = link;
+    }
+    json[data[data.length - 1].children[0].value] = videos;
+    return json;
+  }
+
   async readXml() {
     const asset = Expo.Asset.fromModule(require('../xml/lectures.xml'));
     const text = await (await fetch(asset.uri)).text();
     // console.log('contents: ', text);
     var xml = new XMLParser().parseFromString(text);
     // var file = await FS.readAsStringAsync('./xml/lectures.txt', {});
-
+    // console.log(xml);
     var curr = 0;
     var c = xml.children;
     while (c[curr]) {
       var n = c[curr].children;
+      var json = this._createJSON(n);
       var newArray = this.state.db.slice();
-      newArray.push(n);
+      newArray.push(json);
       this.setState({
         dataSource: this.state.dataSource.cloneWithRows(newArray),
         db: newArray,
@@ -79,7 +103,7 @@ class WeekScreen extends React.Component {
                 style={styles.weekImage}
                 source={require('../assets/harvard.jpg')}>
                 <Text style={styles.weekText}>
-                  {rowData[0].value}
+                  {rowData.title}
                 </Text>
               </Image>
             </TouchableHighlight>
