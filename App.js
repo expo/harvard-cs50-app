5ce70c1122a4fec4c367a3ea49144bf856cce238
Abi Raja <abimanyuraja@gmail.com>
2017-07-09 12:37:15 -0700

    Cache images before app load

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StackNavigator } from 'react-navigation';
+import { AppLoading } from 'expo';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+
 import HomeScreen from './screens/HomeScreen';
 import WeekScreen from './screens/WeekScreen';
 import LinkScreen from './screens/LinkScreen';
 
-export default StackNavigator({
+const AppNavigator = StackNavigator({
   Home: {
     screen: HomeScreen,
     headerTintColor: 'red',
@@ -16,3 +20,47 @@ export default StackNavigator({
     headerMode: 'float',
   },
 });
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Expo.Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Expo.Font.loadAsync(font));
+}
+
+class AppContainer extends React.Component {
+  state = {
+    appIsReady: false,
+  };
+
+  componentWillMount() {
+    this._loadAssetsAsync();
+  }
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([require('./assets/harvard.jpg')]);
+
+    const fontAssets = cacheFonts([]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+
+    this.setState({ appIsReady: true });
+  }
+
+  render() {
+    if (!this.state.appIsReady) {
+      return <AppLoading />;
+    }
+
+    return <AppNavigator />;
+  }
+}
+
+export default AppContainer;
