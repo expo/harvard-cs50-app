a21ada7fbea722260883511aa094dd2e56f5b4b4
Abi Raja <abimanyuraja@gmail.com>
2017-07-08 17:31:42 -0700

    organize lecture screen rendering better

--- screens/LectureScreen.js
+++ screens/LectureScreen.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Video } from 'expo';
-import {
-  StyleSheet,
-  Dimensions,
-  Text,
-  View,
-  Platform,
-  TouchableHighlight,
-} from 'react-native';
+import _ from 'lodash';
+import { Text, View, Platform, TouchableHighlight } from 'react-native';
+
+class VideoSection extends React.Component {
+  render() {
+    console.log('SOURCES : ', this.props.sources);
+    return (
+      <View
+        style={{
+          flex: 1,
+          padding: 10,
+          marginTop: 80,
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: 300,
+          height: 300,
+          borderColor: 'black',
+          borderWidth: 2,
+        }}>
+        <Video
+          source={{
+            uri: this.props.sources['240p'],
+          }}
+          resizeMode={Video.RESIZE_MODE_CONTAIN}
+          style={{ width: 300, height: 300 }}
+          shouldPlay={true}
+        />
+      </View>
+    );
+  }
+}
 
 class LectureScreen extends React.Component {
   state = {
@@ -29,6 +52,7 @@ class LectureScreen extends React.Component {
     this.props.navigation.navigate('Web', { url: url });
   };
 
+  // TODO: Move this to teh data processing layer
   _getPSetURL(url) {
     var length = url.length;
     var week = url.charAt(length - 6);
@@ -56,15 +80,24 @@ class LectureScreen extends React.Component {
 
   render() {
     const { params } = this.props.navigation.state;
-    //this.setVideoIndex(params);
+    const data = params.data;
 
-    const linkStyle = {
-      fontSize: 25,
-      color: 'black',
-      fontWeight: 'bold',
-      backgroundColor: 'transparent',
-      alignSelf: 'center',
-    };
+    var linkKeys = ['slides', 'source code', 'notes'];
+    var links = _.pickBy(data, (v, k) => linkKeys.includes(k));
+
+    const Link = props =>
+      <TouchableHighlight
+        onPress={() => {
+          this.onButtonPress(props.url);
+        }}>
+        <Text
+          style={{
+            fontSize: 20,
+            color: 'black',
+          }}>
+          {props.name}
+        </Text>
+      </TouchableHighlight>;
 
     return (
       <View
@@ -73,51 +106,11 @@ class LectureScreen extends React.Component {
           justifyContent: 'center',
           paddingTop: 20,
         }}>
-        <View
-          style={{
-            flex: 1,
-            padding: 10,
-            marginTop: 80,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Video
-            source={{
-              uri: params.data.Videos['240p'],
-            }}
-            resizeMode={Video.RESIZE_MODE_CONTAIN}
-            style={{ width: 300, height: 300 }}
-            shouldPlay={true}
-          />
-        </View>
-        <TouchableHighlight
-          onPress={() => {
-            this.onButtonPress(params.data.Notes);
-          }}>
-          <Text style={linkStyle}>
-            Notes for {params.data.title}
-          </Text>
-        </TouchableHighlight>
-        <Text />
-        <Text />
-        <TouchableHighlight
-          onPress={() => {
-            this.onButtonPress(this._getPSetURL(params.data.Notes));
-          }}>
-          <Text style={linkStyle}>
-            Problem Set for {params.data.title}
-          </Text>
-        </TouchableHighlight>
-        <Text />
-        <Text />
-        <TouchableHighlight
-          onPress={() => {
-            this.onButtonPress(params.data['Source Code']);
-          }}>
-          <Text style={linkStyle}>
-            Source Code for {params.data.title}
-          </Text>
-        </TouchableHighlight>
+        <VideoSection sources={data.videos} />
+        <Text> course materials</Text>
+        {_.map(links, (url, name) => {
+          return <Link key={url} name={name} url={url} />;
+        })}
       </View>
     );
   }
