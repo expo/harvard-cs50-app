ead3ba977ef0611edd99fa9fbda8e245638b0e12
Abi Raja <abimanyuraja@gmail.com>
2017-07-26 04:11:08 -0700

    Add Lottie animations

--- screens/OnboardScreen.js
+++ screens/OnboardScreen.js
@@ -10,14 +10,34 @@ import {
   Text,
 } from 'react-native';
 import Swiper from 'react-native-swiper';
-import Expo from 'expo';
+import Expo, { DangerZone } from 'expo';
+const { Lottie } = DangerZone;
 import { colors, fontSize } from '../styles/style';
 
 // <StatusBar style what?
 
 class OnboardScreen extends React.Component {
+  state = {
+    animation: null,
+  };
+
+  componentWillMount() {
+    this._loadAnimationAsync();
+  }
+
+  _loadAnimationAsync = async () => {
+    let result = await fetch(
+      'https://cdn.rawgit.com/airbnb/lottie-react-native/635163550b9689529bfffb77e489e4174516f1c0/example/animations/Watermelon.json'
+    );
+
+    this.setState({ animation: JSON.parse(result._bodyText) }, () => {
+      this.animation.reset();
+      this.animation.play();
+    });
+  };
+
   render() {
-    const Panel = ({ text, style }) =>
+    const Panel = ({ text, style, animated }) =>
       <View
         style={[
           {
@@ -28,6 +48,19 @@ class OnboardScreen extends React.Component {
           },
           style,
         ]}>
+        {animated &&
+          this.state.animation &&
+          <Lottie
+            ref={animation => {
+              this.animation = animation;
+            }}
+            style={{
+              width: 400,
+              height: 400,
+              backgroundColor: colors.tertiary,
+            }}
+            source={this.state.animation}
+          />}
         <Text
           style={{
             color: colors.primary,
@@ -44,6 +77,7 @@ class OnboardScreen extends React.Component {
     return (
       <Swiper loop={false} activeDotColor={colors.primary}>
         <Panel
+          animated={true}
           style={{ backgroundColor: colors.tertiary }}
           text="Watch lectures and access course materials"
         />
