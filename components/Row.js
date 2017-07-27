5ecda63f1e8c343278f7ad21765e60013486554b
Jeffrey Da <jexelD@gmail.com>
2017-07-27 16:00:55 -0700

    Add native ripple effect to Row.js

--- components/Row.js
+++ components/Row.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Text, TouchableHighlight, View, Platform } from 'react-native';
+import {
+  Text,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  View,
+  Platform,
+} from 'react-native';
 import styles, { colors, fontSize } from '../styles/style';
 import { EvilIcons } from '@expo/vector-icons';
 
@@ -57,15 +63,7 @@ class Row extends React.Component {
       );
     } else {
       return (
-        <TouchableHighlight
-          onPress={this.props.onPress}
-          underlayColor={colors.primary}
-          onPressIn={() => {
-            this.setState({ pressed: true });
-          }}
-          onPressOut={() => {
-            this.setState({ pressed: false });
-          }}
+        <View
           style={{
             justifyContent: 'center',
             paddingTop: 20,
@@ -74,31 +72,45 @@ class Row extends React.Component {
             borderBottomWidth: 1,
             borderBottomColor: colors.primary,
           }}>
-          <View
+          <TouchableNativeFeedback
+            onPress={this.props.onPress}
+            underlayColor={colors.primary}
+            onPressIn={() => {
+              this.setState({ pressed: true });
+            }}
+            onPressOut={() => {
+              this.setState({ pressed: false });
+            }}
             style={{
-              justifyContent: 'space-between',
-              flexDirection: 'row',
+              justifyContent: 'center',
+              paddingTop: 20,
+              paddingBottom: 20,
+              marginLeft: styles.mainViewStyle.marginLeft,
+              borderBottomWidth: 1,
+              borderBottomColor: colors.primary,
             }}>
-            <Text
+            <View
               style={{
-                fontFamily: 'roboto-light',
-                fontSize: fontSize(1),
-                color: this.state.pressed
-                  ? colors.complementary
-                  : colors.secondary,
-                alignSelf: 'flex-start',
+                justifyContent: 'space-between',
+                flexDirection: 'row',
               }}>
-              {this.props.text}
-            </Text>
-            <EvilIcons
-              name="chevron-right"
-              size={32}
-              color={
-                this.state.pressed ? colors.complementary : colors.secondary
-              }
-            />
-          </View>
-        </TouchableHighlight>
+              <Text
+                style={{
+                  fontFamily: 'roboto-light',
+                  fontSize: fontSize(1),
+                  color: colors.secondary,
+                  alignSelf: 'flex-start',
+                }}>
+                {this.props.text}
+              </Text>
+              <EvilIcons
+                name="chevron-right"
+                size={32}
+                color={colors.secondary}
+              />
+            </View>
+          </TouchableNativeFeedback>
+        </View>
       );
     }
   }
