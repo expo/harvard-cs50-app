83139f822ab9f638570aee1139ec574f67653b67
Jeffrey Da <jexelD@gmail.com>
2017-07-27 16:33:46 -0700

    Add native ripple effect to Android for WeekBox.js

--- components/WeekBox.js
+++ components/WeekBox.js
@@ -18,48 +18,93 @@ class WeekBox extends React.Component {
 
   render() {
     const textStyle = { color: colors.primary, fontSize: fontSize(2) };
-    return (
-      <TouchableHighlight onPress={this.props.onPress}>
-        <View
-          key={this.props.key}
-          style={{
-            paddingTop: 50,
-            paddingBottom: 50,
-            marginTop: 10,
-            borderRadius: 5,
-            borderWidth: 2,
-            borderColor: colors.primary,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}>
+    if (Platform.OS === 'ios') {
+      return (
+        <TouchableHighlight onPress={this.props.onPress}>
           <View
+            key={this.props.key}
             style={{
-              width: this.props.imageWidth,
+              paddingTop: 50,
+              paddingBottom: 50,
+              marginTop: 10,
+              borderRadius: 5,
+              borderWidth: 2,
+              borderColor: colors.primary,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
               alignItems: 'center',
             }}>
-            <Image
-              source={require('../assets/memory.png')}
-              fadeDuration={0}
-              style={{ width: 50, height: 60 }}
-            />
-            {/* <AnimatedIcon /> */}
+            <View
+              style={{
+                width: this.props.imageWidth,
+                alignItems: 'center',
+              }}>
+              <Image
+                source={require('../assets/memory.png')}
+                fadeDuration={0}
+                style={{ width: 50, height: 60 }}
+              />
+              {/* <AnimatedIcon /> */}
+            </View>
+            <View
+              style={{
+                width: this.props.textWidth,
+                alignItems: 'flex-start',
+              }}>
+              <Text style={textStyle} numberOfLines={1}>
+                {this.props.title}
+              </Text>
+              <Text style={textStyle} numberOfLines={1}>
+                {this.props.desc}
+              </Text>
+            </View>
           </View>
+        </TouchableHighlight>
+      );
+    } else {
+      return (
+        <TouchableNativeFeedback onPress={this.props.onPress}>
           <View
+            key={this.props.key}
             style={{
-              width: this.props.textWidth,
-              alignItems: 'flex-start',
+              paddingTop: 50,
+              paddingBottom: 50,
+              marginTop: 10,
+              borderRadius: 5,
+              borderWidth: 2,
+              borderColor: colors.primary,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
             }}>
-            <Text style={textStyle} numberOfLines={1}>
-              {this.props.title}
-            </Text>
-            <Text style={textStyle} numberOfLines={1}>
-              {this.props.desc}
-            </Text>
+            <View
+              style={{
+                width: this.props.imageWidth,
+                alignItems: 'center',
+              }}>
+              <Image
+                source={require('../assets/memory.png')}
+                fadeDuration={0}
+                style={{ width: 50, height: 60 }}
+              />
+              {/* <AnimatedIcon /> */}
+            </View>
+            <View
+              style={{
+                width: this.props.textWidth,
+                alignItems: 'flex-start',
+              }}>
+              <Text style={textStyle} numberOfLines={1}>
+                {this.props.title}
+              </Text>
+              <Text style={textStyle} numberOfLines={1}>
+                {this.props.desc}
+              </Text>
+            </View>
           </View>
-        </View>
-      </TouchableHighlight>
-    );
+        </TouchableNativeFeedback>
+      );
+    }
   }
 }
 
