b5c28f6b0f207e0ad32cb3467106bcac2a6f6aef
Abi Raja <abimanyuraja@gmail.com>
2017-08-02 16:47:53 -0700

    Add a close button for the Resources Screen

--- screens/ResourcesScreen.js
+++ screens/ResourcesScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import Row from '../components/Row';
+import CrossTouchable from '../components/CrossTouchable';
+import { colors } from '../styles/style';
 
 const RESOURCES = [
   { name: 'Discuss', url: 'https://cs50.net/discuss' },
@@ -16,11 +18,25 @@ const RESOURCES = [
 ];
 
 class ResourcesScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Resources',
-    headerTintColor: styles.headerTintColor,
-    headerStyle: styles.headerStyle,
-    headerMode: 'float',
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Resources',
+      headerTintColor: styles.headerTintColor,
+      headerStyle: styles.headerStyle,
+      headerMode: 'float',
+      headerRight: (
+        <CrossTouchable
+          style={{ paddingRight: 20 }}
+          hitSlop={{ top: 20, left: 20, bottom: 20, right: 20 }}
+          onPress={() => {
+            // Because this header is in the second-level navigator, to go back to the first-level navigator, we can `null`
+            // to go back anywhere
+            navigation.goBack(null);
+          }}>
+          <Text style={{ color: colors.tertiary }}>Close</Text>
+        </CrossTouchable>
+      ),
+    };
   };
 
   onButtonPress = url => {
