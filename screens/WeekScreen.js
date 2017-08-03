f4d8723590fa6bba8f17ca4f9245efd13601796a
Abi Raja <abimanyuraja@gmail.com>
2017-08-03 12:46:22 -0700

    Create Downloader component to show current status of download

--- screens/WeekScreen.js
+++ screens/WeekScreen.js
@@ -1,13 +1,115 @@
 import React from 'react';
-import { Text, View, Dimensions, TouchableHighlight } from 'react-native';
+import {
+  Text,
+  View,
+  Dimensions,
+  TouchableHighlight,
+  NetInfo,
+} from 'react-native';
 import { ScreenOrientation, FileSystem, Video } from 'expo';
-import Analytics from '../utils/Analytics';
 import _ from 'lodash';
+import * as Progress from 'react-native-progress';
+import reactMixin from 'react-mixin';
+import TimerMixin from 'react-timer-mixin';
+import prettyMs from 'pretty-ms';
 
 import VideoPlayer from '../components/VideoPlayer';
 import Row from '../components/Row';
-
+import Analytics from '../utils/Analytics';
 import styles from '../styles/style';
+
+var STATES = {
+  NOTSTARTED: 1,
+  DOWNLOADING: 2,
+  STALLED: 3,
+  DOWNLOADED: 4,
+};
+
+class Downloader extends React.Component {
+  state = {
+    progress: 0,
+    timeRemaining: '10 hours',
+    state: STATES.NOTSTARTED,
+    totalBytes: 10000,
+    currentBytes: 0,
+  };
+
+  constructor(props) {
+    super(props);
+    NetInfo.fetch().then(reach => {
+      console.log('Initial: ' + reach);
+    });
+    NetInfo.addEventListener('change', reach => {
+      console.log('Change: ' + reach);
+      // TODO: Change to STATES.STALLED
+    });
+  }
+
+  saveToDisk() {
+    this.setState({ state: STATES.DOWNLOADING });
+
+    this.setInterval(() => {
+      let currentBytes = this.state.currentBytes;
+      const totalBytes = this.state.totalBytes;
+
+      currentBytes = currentBytes + 500;
+      if (currentBytes > totalBytes) {
+        currentBytes = totalBytes;
+      }
+
+      const speedBytesPerMs = 1;
+      const timeRemainingMs = (totalBytes - currentBytes) / speedBytesPerMs;
+
+      this.setState({
+        progress: currentBytes / totalBytes,
+        currentBytes: currentBytes,
+        timeRemaining: prettyMs(timeRemainingMs),
+        state: this.state.progress < 1 ? STATES.DOWNLOADING : STATES.DOWNLOADED,
+      });
+    }, 500);
+  }
+
+  render() {
+    return (
+      <View
+        style={{
+          marginLeft: 20,
+          marginRight: 20,
+        }}>
+        {this.state.state === STATES.NOTSTARTED &&
+          <View>
+            <TouchableHighlight onPress={this.saveToDisk.bind(this)}>
+              <Text>save for offline</Text>
+            </TouchableHighlight>
+          </View>}
+        {this.state.state === STATES.DOWNLOADING &&
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}>
+            <View>
+              <Text>Downloading lecture for offline viewing</Text>
+              <Text>
+                {this.state.timeRemaining} remaining
+              </Text>
+            </View>
+            <View>
+              <Progress.Circle size={30} progress={this.state.progress} />
+            </View>
+          </View>}
+        {this.state.state === STATES.DOWNLOADED &&
+          <View>
+            <Text>Lecture available for offline viewing</Text>
+          </View>}
+      </View>
+    );
+  }
+}
+
+reactMixin(Downloader.prototype, TimerMixin);
 
 class WeekScreen extends React.Component {
   state = {
@@ -113,22 +215,12 @@ class WeekScreen extends React.Component {
             shouldPlay={true}
           />}
 
-        <View
+        <Downloader
           style={{
-            marginLeft: 20,
-            marginRight: 20,
-          }}>
-          <TouchableHighlight
-            style={{ display: this.state.isPortrait ? 'flex' : 'none' }}
-            onPress={() => {
-              {
-                /* console.log(data.videos.sources); */
-              }
-              this.saveToDisk(data.videos['240p']);
-            }}>
-            <Text>save for offline</Text>
-          </TouchableHighlight>
-        </View>
+            display: this.state.isPortrait ? 'flex' : 'none',
+            marginBottom: 40,
+          }}
+        />
         <View
           style={{
             alignItems: 'flex-start',
